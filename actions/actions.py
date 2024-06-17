677a4828f1694c6efc7e5137faac6fabb8d313b4
Rajpopzein <kraj0132@gmail.com>
2024-06-17 18:44:15 +0530

    added resume link

--- actions/actions.py
+++ actions/actions.py
@@ -27,3 +27,15 @@
 #         return []
 # actions/actions.py
 
+from rasa_sdk import Action
+from rasa_sdk.executor import CollectingDispatcher
+
+class ActionProvideDownloadLink(Action):
+
+    def name(self) -> str:
+        return "action_provide_download_link"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker, domain):
+        download_url = "https://drive.usercontent.google.com/download?id=1RmURQ2OdS28nt8utWMZ9WNMd_TVB7OhU&export=download&authuser=0&confirm=t&uuid=288800af-ebc6-439f-a5a0-0aedd8e0ee58&at=APZUnTUJ0zVstFMGUqsHcL8I12RO:1718627031956"
+        dispatcher.utter_message(text=f"Click [here]({download_url}) resume!")
+        return []
